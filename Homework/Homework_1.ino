ec57c5ae881fc8f5c67cdf5ecab9e61c56c6b3f9
Vartic Mihai-Vlad <61534490+Varothex@users.noreply.github.com>
2021-10-26 22:25:32 +0300

    The final version
    I have updated the code by applying the good practices mentioned in the course guide.

--- Homework/Homework_1.ino
+++ Homework/Homework_1.ino
@@ -1,14 +1,23 @@
-const int redPin = 11;
-const int bluePin = 10;
 const int greenPin = 9;
+const int bluePin = 10;
+const int redPin = 11;
 
 const int potGreen = A0;
 const int potBlue = A1;
 const int potRed = A2;
 
-float potValueGreen = 0.00;
-float potValueBlue = 0.00;
-float potValueRed = 0.00;
+const float minAnalogValue = 0.0;
+const float maxAnalogValue = 1023.0;
+const float minBrightness = 0.0;
+const float maxBrightness = 255.0;
+
+float potValueGreen = 0.0;
+float potValueBlue = 0.0;
+float potValueRed = 0.0;
+
+float trueValueGreen = 0.0;
+float trueValueBlue = 0.0;
+float trueValueRed = 0.0;
 
 void setup() 
 {
@@ -25,12 +34,17 @@ void setup()
 
 void loop() 
 {
-  potValueGreen = analogRead(potGreen) * 255.0 / 1023.0;
-  potValueBlue = analogRead(potBlue) * 255.0 / 1023.0;
-  potValueRed = analogRead(potRed) * 255.0 / 1023.0;
+  potValueGreen = analogRead(potGreen);
+  // I have a common cathode, therefore I must substarct the returned value from the maxim RGB brightness.
+  trueValueGreen = maxBrightness - map(potValueGreen, minAnalogValue, maxAnalogValue, minBrightness, maxBrightness);
 
-  analogWrite(greenPin, 255.00 - potValueGreen);
-  analogWrite(bluePin, 255.00 - potValueBlue);
-  analogWrite(redPin, 255.00 - potValueRed);
-}
+  potValueBlue = analogRead(potBlue);
+  trueValueBlue = maxBrightness - map(potValueBlue, minAnalogValue, maxAnalogValue, minBrightness, maxBrightness);
   
+  potValueRed = analogRead(potRed);
+  trueValueRed = maxBrightness - map(potValueRed, minAnalogValue, maxAnalogValue, minBrightness, maxBrightness);
+
+  analogWrite(greenPin, trueValueGreen);
+  analogWrite(bluePin, trueValueBlue);
+  analogWrite(redPin, trueValueRed);
+}
