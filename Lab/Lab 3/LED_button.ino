94fe58d197822075a81ae352263a7926ab7b7897
Vartic Mihai-Vlad <61534490+Varothex@users.noreply.github.com>
2021-11-10 14:42:17 +0200

    Create LED_button.ino

---
+++ Lab/Lab 3/LED_button.ino
@@ -0,0 +1,20 @@
+const int pushButton = 2;
+bool buttonState = LOW;
+
+const int ledPin = 8;
+bool ledState = LOW;
+
+void setup() 
+{
+  pinMode(pushButton, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  buttonState = digitalRead(pushButton);
+  ledState = !buttonState;
+  digitalWrite(ledPin, ledState);
+  
+}
